1a3cbc73a11b7b5307a7e00f2c5e082ba462ec2b
Conferencia Ejércitos Americanos (CEA) <developer@redcea.onmicrosoft.com>
2023-09-18 14:23:35 -0500

    Remove Imports not in use

--- webparts/carousel/src/webparts/imageCarousel/components/ImageCarouselCaption.tsx
+++ webparts/carousel/src/webparts/imageCarousel/components/ImageCarouselCaption.tsx
@@ -1,21 +1,20 @@
 import * as React from 'react';
-import styles from './ImageCarousel.module.scss';
 import Carousel from 'react-bootstrap/Carousel';
 import "bootstrap/dist/css/bootstrap.css";
 import { escape } from '@microsoft/sp-lodash-subset';
 
-function CreateCarouselCaption(props) {
-   
+function CreateCarouselCaption(props): any{
+
     if (props.Title === undefined && props.Description === undefined) {
         return (
-            <Carousel.Caption className="d-none"/>
+            <Carousel.Caption className="d-none" />
         );
     }
     else {
         return (
             <Carousel.Caption className="d-none d-md-block">
-                    {(props.Title !== undefined)? <h5>{escape(props.Title)}</h5>: ""}
-                    {(props.Description !== undefined)? <p>{escape(props.Description)}</p>: ""}
+                {(props.Title !== undefined) ? <h5>{escape(props.Title)}</h5> : ""}
+                {(props.Description !== undefined) ? <p>{escape(props.Description)}</p> : ""}
             </Carousel.Caption>
         );
     }//else 
