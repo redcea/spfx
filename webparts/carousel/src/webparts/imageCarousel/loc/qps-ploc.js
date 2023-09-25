99ad1bee1ddeff29ef02862082ddd77a28deb54b
Conferencia Ejércitos Americanos (CEA) <developer@redcea.onmicrosoft.com>
2023-09-25 13:52:20 -0500

    Add missing Fields

--- webparts/carousel/src/webparts/imageCarousel/loc/qps-ploc.js
+++ webparts/carousel/src/webparts/imageCarousel/loc/qps-ploc.js
@@ -1,8 +1,21 @@
 define([], function() {
     return {
       "PropertyPaneDescription": "[!!! Gřèèƭïñϱ ωèβ ƥářƭ çôñƒïϱúřáƭïôñ ℓôřè₥ ïƥƨú !!!]",
-      "DisplayGroupName": "[!!! Ðïƨƥℓá¥ ℓ !!!]",
-      "GreetingFieldLabel": "[!!! Gřèèƭïñϱ ƭô ƨλôω ïñ ƭλè ωèβ ƥářƭ ℓôřè₥ ïƥƨú !!!]",
-      "LearnMoreButtonLabel": "[!!! £èářñ ₥ôřè ℓôř !!!]"
+      "BasicGroupName": "[!!! Ðïƨƥℓá¥ ℓ !!!]",
+      "DescriptionFieldLabel": "",
+      "ListName": "[!!! Ðïƨƥℓá¥ ℓ !!!]",
+      "slideSpeed": "[!!! Gřèèƭïñϱ ƭô ƨλôω ïñ ƭλè ωèβ ƥářƭ ℓôřè₥ ïƥƨú !!!]",
+      "OrderFieldLabel": "[!!! £èářñ ₥ôřè ℓôř !!!]",
+      "OrderFieldAscendingOptionLabel": "[!!! £èářñ ₥ôřè ℓôř !!!]",
+      "OrderFieldDescendingOptionLabel": "[!!! £èářñ ₥ôřè ℓôř !!!]",
+      "NumberOfItemsFieldLabel": "[!!! £èářñ ₥ôřè ℓôř !!!]",
+      "LanguageFieldLabel": "[!!! £èářñ ₥ôřè ℓôř !!!]",
+      "EnglishOptionLabel": "[!!! £èářñ ₥ôřè ℓôř !!!]",
+      "PortugueseOptionLabel": "[!!! £èářñ ₥ôřè ℓôř !!!]",
+      "SpanishOptionLabel": "[!!! £èářñ ₥ôřè ℓôř !!!]",
+      "NeedConfigurationHeading": "[!!! £èářñ ₥ôřè ℓôř !!!]",
+      "NeedConfigurationSubHeading": "[!!! £èářñ ₥ôřè ℓôř !!!]",
+      "ShowItemsFromSelectedList": "[!!! Gřèèƭïñϱ ƭô ƨλôω ïñ ƭλè ωèβ ƥářƭ ℓôřè₥ ïƥƨú !!!]",
+      "PauseCarouselOptionLabel": "[!!! Gřèèƭïñϱ ƭô ƨλôω ïñ ƭλè ωèβ ƥářƭ ℓôřè₥ ïƥƨú !!!]"
     }
   });
