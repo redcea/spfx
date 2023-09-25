52cd1b342b9abb1eaefc29e93bb4aa289125f83d
Conferencia Ejércitos Americanos (CEA) <developer@redcea.onmicrosoft.com>
2023-09-25 13:16:17 -0500

    Spanish Translation Corrections

--- webparts/carousel/src/webparts/imageCarousel/loc/es-es.js
+++ webparts/carousel/src/webparts/imageCarousel/loc/es-es.js
@@ -1,20 +1,21 @@
-define([], function() {
-    return {
-      "PropertyPaneDescription": "CEA - 365 Slideshow",
-      "BasicGroupName": "Fuente de Datos",
-      "DescriptionFieldLabel": "",
-      "ListName": "Nombre de la Lista para el Slideshow",
-      "slideSpeed": "Especifica el retardo (en segundos) entre cada diapositiva",
-      "OrderFieldLabel": "Orden de las Imagenes",
-      "OrderFieldAscendingOptionLabel": "ascendiente",
-      "OrderFieldDescendingOptionLabel": "descendente",
-      "NumberOfItemsFieldLabel": "Número de fotos a mostrar",
-      "LanguageFieldLabel": "Mostrar idioma",
-      "EnglishOptionLabel": "Inglés",
-      "PortugueseOptionLabel": "Portugués",
-      "SpanishOptionLabel": "Español",
-      "NeedConfigurationHeading": "Slidehow de Imagenes",
-      "ShowItemsFromSelectedList": "Mostrar slideshow de imágenes de la lista seleccionada",
-      "PauseCarouselOptionLabel": "Pone en pausa el slideshow cuando el puntero del ratón entra en él."    
-    }
-  });
+define([], function () {
+  return {
+    "PropertyPaneDescription": "CEA - 365 Carrusel",
+    "BasicGroupName": "Fuente de Datos",
+    "DescriptionFieldLabel": "",
+    "ListName": "Nombre de la Lista de el Carrusel",
+    "slideSpeed": "Especifica el retardo (en segundos) entre cada diapositiva",
+    "OrderFieldLabel": "Orden de las Imagenes",
+    "OrderFieldAscendingOptionLabel": "ascendiente",
+    "OrderFieldDescendingOptionLabel": "descendente",
+    "NumberOfItemsFieldLabel": "Número de fotos a mostrar",
+    "LanguageFieldLabel": "Mostrar idioma",
+    "EnglishOptionLabel": "Inglés",
+    "PortugueseOptionLabel": "Portugués",
+    "SpanishOptionLabel": "Español",
+    "NeedConfigurationHeading": "¡CEA - 365 Carrusel de Imagenes!",
+    "NeedConfigurationSubHeading": "Elemento web no está configurado.",
+    "ShowItemsFromSelectedList": "Para mostrar imágenes en el carrusel, configure el nombre de la lista",
+    "PauseCarouselOptionLabel": "Pone en pausa el slideshow cuando el puntero del ratón entra en él."
+  }
+});
