ddfebe8c8b885c0d54db4a7917482f68f0fab959
Conferencia Ejércitos Americanos (CEA) <developer@redcea.onmicrosoft.com>
2023-09-25 13:11:39 -0500

    pseudo-locale

---
+++ webparts/carousel/src/webparts/imageCarousel/loc/qps-ploc.js
@@ -0,0 +1,8 @@
+define([], function() {
+    return {
+      "PropertyPaneDescription": "[!!! Gřèèƭïñϱ ωèβ ƥářƭ çôñƒïϱúřáƭïôñ ℓôřè₥ ïƥƨú !!!]",
+      "DisplayGroupName": "[!!! Ðïƨƥℓá¥ ℓ !!!]",
+      "GreetingFieldLabel": "[!!! Gřèèƭïñϱ ƭô ƨλôω ïñ ƭλè ωèβ ƥářƭ ℓôřè₥ ïƥƨú !!!]",
+      "LearnMoreButtonLabel": "[!!! £èářñ ₥ôřè ℓôř !!!]"
+    }
+  });
